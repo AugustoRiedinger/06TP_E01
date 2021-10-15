212b3a933ef697858bf554f9a998824b313ad2cf
Augusto Riedinger <ariedinger4@duck.com>
2021-10-15 20:58:41 -0300

    PRUEBA 4
    - Se agregaron los algoritmos para utilizar las funciones de string.h.

--- src/main.c
+++ src/main.c
@@ -3,14 +3,13 @@
   * @author  G. Garcia & A. Riedinger
   * @version 0.1
   * @date    04-10-21
-  * @brief   Envío de datos a través del protocolo RS232 a una PC
-  * 		 por un cable RS232-USB..
+  * @brief   Recepción de datos a través del protocolo RS232 desde una PC
+  * 		 por un cable RS232-USB.
 
   * SALIDAS:
   	  *	LCD  	Conexion Estandar TPs
-	  * RS232 	TX - PCXX
   * ENTRADAS:
-  	  * UserBut PC13
+  	  * RS232 	RX - PA3
 ********************************************************************************/
 
 /*------------------------------------------------------------------------------
@@ -30,12 +29,15 @@ DEFINICIONES:
 #define RX		GPIO_Pin_3
 
 /*Velocidad de trabajo del UART:*/
-#define BaudRate 115200
+#define BaudRate 9600
+
+/*Maximo tamaño de palabra:*/
+#define MaxDataBits 8
 
 /*------------------------------------------------------------------------------
 VARIABLES GLOBALES:
 ------------------------------------------------------------------------------*/
-/*Definicion de los pines del LCD: */
+/*Definicion de los pines del LCD:*/
 LCD_2X16_t LCD_2X16[] = {
 			// Name  , PORT ,   PIN      ,         CLOCK       ,   Init
 			{ TLCD_RS, GPIOC, GPIO_Pin_10, RCC_AHB1Periph_GPIOC, Bit_RESET },
@@ -46,10 +48,14 @@ LCD_2X16_t LCD_2X16[] = {
 			{ TLCD_D7, GPIOF, GPIO_Pin_7,  RCC_AHB1Periph_GPIOF, Bit_RESET }, };
 
 /*Variable para almacenamiento de datos recibidos:*/
-char Data;
+char  CharRec[MaxDataBits];
+
+/*Variable para concatenar los caracteres recibidos en una cadena:*/
+/*Tamaño es la cantidad maxima de caracteres a recibir (1000) por el maximo de palabra mas margen de seguridad:*/
+char  DataString[1200*MaxDataBits];
 
 /*Variable para contar los caracteres:*/
-uint32_t Ch = 0;
+int   StringLength = 0;
 
 /*Variable para contar el tiempo de lectura:*/
 float OpTime = 0;
@@ -67,7 +73,7 @@ CONFIGURACION DEL MICRO:
 	/*Inicializacion del puerto serie en el pin:*/
 	INIT_USART_RX(RX_Port, RX, BaudRate);
 
-	//Inicialización del TIM3 para refresco del LCD:
+	/*Inicialización del TIM3 para refresco del LCD:*/
 	INIT_TIM3();
 	SET_TIM3(TimeBase, Freq);
 
@@ -76,18 +82,20 @@ BUCLE PRINCIPAL:
 ------------------------------------------------------------------------------*/
     while(1)
     {
-    	/*Dato recibido:*/
-		if (USART_GetFlagStatus(USART2, USART_FLAG_RXNE) != RESET)
-		{
-			/*Se guarda lo recibido en la varibale Data:*/
-			Data = USART_ReceiveData(USART2);
+    	/*Si se recibe un dato en el puerto serie:*/
+		if (USART_GetFlagStatus(USART2, USART_FLAG_RXNE) != RESET) {
+			/*Se guarda lo recibido:*/
+			CharRec[0] = USART_ReceiveData(USART2);
 
-			/*Se utiliza un '#' para indicar final de cadena:*/
-			if (Data != '#')
-				Ch++;
+			/*Se concatena el caracter recibido con los anteriores para formar la cadena:*/
+			strcat(DataString,CharRec);
 		}
 
-		OpTime = (float) Ch / BaudRate;
+		/*Calculo de la cantidad de caracteres en la cadena:*/
+		StringLength = strlen(DataString);
+
+		/*Calculo del tiempo de operacion:*/
+		OpTime = (float) StringLength / BaudRate;
     }
 }
 /*------------------------------------------------------------------------------
@@ -100,25 +108,25 @@ void TIM3_IRQHandler(void)
 		TIM_ClearITPendingBit(TIM3, TIM_IT_CC1);
 
 		/*Buffers para almacenamiento de datos:*/
-		char BufferData[BufferLength];
-		char BufferCh[BufferLength];
+		char BufferStringLength[1200*MaxDataBits];
+		char BufferCharRec[MaxDataBits];
 		char BufferOpTime[BufferLength];
 
 		/*Refresco del LCD: */
 		CLEAR_LCD_2x16(LCD_2X16);
 
 		/*Copiar datos a los buffers para imprimir:*/
-		sprintf(BufferData, "%c", Data);
-		sprintf(BufferCh, "%d", Ch);
+		sprintf(BufferCharRec, "%c", CharRec[0]);
+		sprintf(BufferStringLength, "%d", StringLength);
 		sprintf(BufferOpTime, "%.2f", OpTime);
 
 		/*Mensaje para indicar el ultimo caracter leido:*/
 		PRINT_LCD_2x16(LCD_2X16, 0, 0, "Ultimo char: ");
-		PRINT_LCD_2x16(LCD_2X16, 13, 0, BufferData);
+		PRINT_LCD_2x16(LCD_2X16, 13, 0, BufferCharRec);
 
 		/*Mensaje para indicar la cantidad de caracteres leidos:*/
 		PRINT_LCD_2x16(LCD_2X16, 0, 1, "Cant:");
-		PRINT_LCD_2x16(LCD_2X16, 5, 1, BufferCh);
+		PRINT_LCD_2x16(LCD_2X16, 5, 1, BufferStringLength);
 
 		/*Mensaje para indicar el tiempo de operacion.*/
 		PRINT_LCD_2x16(LCD_2X16, 10, 1, "T:");
